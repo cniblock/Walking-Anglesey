0ff21a1e5f4422ee6aa18b11b93bf7fc1e39656b
cniblock <nibbo1984@hotmail.co.uk>
2024-05-09 16:59:48 +0000

    readies code for deploy

--- walkanglesey/settings.py
+++ walkanglesey/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9cd4*zsu7^gc*00g3vzw4k+v@!^ih5p)pmmwe0h2)c&*jcqsts"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8000-cniblock-walking-anglese-jzrwbh89wj.us1.codeanyapp.com"]
+ALLOWED_HOSTS = ["8000-cniblock-walking-anglese-jzrwbh89wj.us1.codeanyapp.com",'.herokuapp.com']
 
 
 # Application definition
