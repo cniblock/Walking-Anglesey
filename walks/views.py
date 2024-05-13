8c89e613583c26c708e82219b17d782e48c4d534
cniblock <nibbo1984@hotmail.co.uk>
2024-05-13 16:04:18 +0000

    added base and index html

--- walks/views.py
+++ walks/views.py
@@ -5,4 +5,5 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "walks/index.html"
+    paginate_by = 6
