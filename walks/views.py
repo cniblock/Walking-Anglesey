c7ae8c4bf15776b1f503cf6f8ad5a584bda4a3a5
cniblock <nibbo1984@hotmail.co.uk>
2024-05-16 14:21:36 +0000

    added post_detail view

--- walks/views.py
+++ walks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -7,3 +7,27 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "walks/index.html"
     paginate_by = 6
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`walks.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`walks.Post`.
+
+    **Template:**
+
+    :template:`walks/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "walks/post_detail.html",
+        {"post": post},
+    )
