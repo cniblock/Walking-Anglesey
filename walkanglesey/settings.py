13512283624433512de1713db3eda7171d16aaa4
cniblock <nibbo1984@hotmail.co.uk>
2024-05-23 14:32:04 +0000

    enable serving of image files

--- walkanglesey/settings.py
+++ walkanglesey/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-cniblock-walking-anglese-jzrwbh89wj.us1.codeanyapp.com",'.herokuapp.com']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     "about",
     "walks",
 ]
