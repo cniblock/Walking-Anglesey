ceb26dc31344b21c229c78a85a1901e36810cd44
cniblock <nibbo1984@hotmail.co.uk>
2024-06-01 14:28:36 +0000

    Updated settings for Heroku deployment

--- walkanglesey/settings.py
+++ walkanglesey/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-cniblock-walking-anglese-v7zv8333cx.us1.codeanyapp.com",'.herokuapp.com']
 
@@ -153,6 +153,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
