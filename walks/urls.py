29817a3b3a80988870ab540de1811dd5907e8dcb
cniblock <nibbo1984@hotmail.co.uk>
2024-06-02 22:52:32 +0000

    removed all likes functions and migrated

--- walks/urls.py
+++ walks/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-from .views import subscribe_newsletter, like_post
+from .views import subscribe_newsletter
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
@@ -9,5 +9,4 @@ urlpatterns = [
     path('<slug:slug>/', views.post_detail, name='post_detail'), 
     path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete),
-    path('like/<int:post_id>/', like_post, name='like_post'),
 ]
