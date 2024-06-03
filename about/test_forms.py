ed743e032622b9151284cc52351f72b4582cd2e2
cniblock <nibbo1984@hotmail.co.uk>
2024-06-03 12:27:14 +0000

    add test for about_views

--- about/test_forms.py
+++ about/test_forms.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
 from .forms import CollaborateForm
 
 
--- about/views.py
+++ about/views.py
@@ -3,18 +3,20 @@ from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
-# Create your views here.
 def about_me(request):
-    
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
             messages.add_message(request, 
             messages.SUCCESS, "Collaboration request received! We will endeavour to respond within 2 working days.")
+        else:
+            messages.add_message(request, 
+            messages.ERROR, "There was an error with your submission. Please check the form and try again.")
+    else:
+        collaborate_form = CollaborateForm()
 
     about = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm()
 
     return render(
         request,
