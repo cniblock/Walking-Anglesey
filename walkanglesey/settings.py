ccb9aca7ae8e3a0f286b1d1952afda2215455481
cniblock <nibbo1984@hotmail.co.uk>
2024-06-03 16:25:52 +0000

    debug true after deployment

--- walkanglesey/settings.py
+++ walkanglesey/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-cniblock-walking-anglese-v7zv8333cx.us1.codeanyapp.com",'.herokuapp.com']
 
