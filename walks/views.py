c8d9aecabdcb2aa0d6d6c1e659433a845115cbf8
Chris Niblock <nibbo1984@hotmail.co.uk>
2024-09-20 16:09:02 +0000

    Enhance comment section: Add edit/delete functionality and comment display improvements

--- walks/views.py
+++ walks/views.py
@@ -86,12 +86,13 @@ def comment_delete(request, slug, comment_id):
     post = get_object_or_404(queryset, slug=slug)
     comment = get_object_or_404(Comment, pk=comment_id)
 
-    if comment.author == request.user:
-        comment.delete()
-        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
-    else:
-        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+    if comment.author != request.user:
+        messages.add_message(request, messages.ERROR, 'You cannot delete comments that are not yours!')
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
+    comment.delete()
+    messages.add_message(request, messages.SUCCESS, 'Comment deleted successfully!')
+    
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 def walkslist(request):
