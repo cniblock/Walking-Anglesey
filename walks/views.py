5f8eb0918291bc452c90c97a90f3ba2d78a920d1
Chris Niblock <nibbo1984@hotmail.co.uk>
2024-09-21 14:15:54 +0000

    add custom error views to views.py

--- walks/views.py
+++ walks/views.py
@@ -9,6 +9,15 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 import random
 
+def custom_403_view(request, exception):
+    return render(request, "403.html", {}, status=403)
+
+def custom_404_view(request, exception):
+    return render(request, "404.html", {}, status=404)
+
+def custom_500_view(request):
+    return render(request, "500.html", status=500)
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('?')
     template_name = "walks/index.html"
